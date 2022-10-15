c2ecc2d9ee24ecdcd6ab7eff3e0fcbe1377cbaab
unknown <km123abca>
2022-10-15 14:15:06 +0530

    commit made at 14:15:05.99 date:15-10-2022

--- Javascript/PlatformGame/scenedata.js
+++ Javascript/PlatformGame/scenedata.js
@@ -1,6 +1,8 @@
 let scenes=[
 			[
 			    {"sprite":"thorn","x":1870,"y":924,"width":600,"height":250,"xoffset":0,"yoffset":0},
+			    {"sprite":"shrub","x":200,"y":924,"width":2400,"height":250,"xoffset":0,"yoffset":0},
+			    {"sprite":"shrub","x":3675,"y":924,"width":2400,"height":250,"xoffset":0,"yoffset":0},
 				{"sprite":"tree","x":-950,"y":424,"width":361,"height":343},
 				{"sprite":"platform","x":-150,"y":600,"width":1987,"height":50},
 				{"sprite":"platform","x":1459,"y":479,"width":1200,"height":50},
@@ -22,6 +24,8 @@ let scenes=[
 			    // {"sprite":"player","x":200,"y":200,"width":150,"height":100},
 			    // {"sprite":"lance","x":1060,"y":200,"width":250,"height":200,spriteId:"lance1"},
 			    // {"sprite":"husk","x":1062,"y":500,"width":150,"height":200,"spriteId":"husk2"},
+			    {"sprite":"thorn","x":-400,"y":824,"width":600,"height":250,"xoffset":0,"yoffset":0},
+			    {"sprite":"shrub","x":713,"y":870,"width":1571,"height":217,"xoffset":0,"yoffset":0},
 				{"sprite":"platform","x":563,"y":600,"width":2200,"height":50},				
 				{"sprite":"camstopper_left","x":-115,"y":394},
 				{"sprite":"camstopper_right","x":925,"y":447},
@@ -31,7 +35,12 @@ let scenes=[
 				{"sprite":"scenechanger_scright","x":1637,"y":468},
 				{"sprite":"MarkedSpot_enemystopleft","x":280,"y":523}
 			],
-			[			    
+			[	
+			    {"sprite":"shrub","x":-425,"y":994,"width":1111,"height":250,"xoffset":0,"yoffset":0},
+			    {"sprite":"shrub","x":1475,"y":994,"width":1312,"height":171,"xoffset":0,"yoffset":0},
+			     {"sprite":"shrub","x":3138,"y":994,"width":1312,"height":171,"xoffset":0,"yoffset":0},
+			    {"sprite":"thorn","x":400,"y":947,"width":736,"height":250,"xoffset":0,"yoffset":0},
+			    {"sprite":"thorn","x":2338,"y":1000,"width":948,"height":250,"xoffset":0,"yoffset":0},		    
 				{"sprite":"platform","x":-12,"y":600,"width":1625,"height":50},		
 				{"sprite":"platform","x":1350,"y":770,"width":1563,"height":50},	
 				{"sprite":"platform","x":2651,"y":600,"width":1238,"height":50},			
@@ -43,14 +52,18 @@ let scenes=[
 				{"sprite":"scenechanger_scright","x":3238,"y":460},
 				{"sprite":"MarkedSpot_fkjumpup","x":1882,"y":685},
 				{"sprite":"MarkedSpot_fkjumpup","x":895,"y":685},
-				{"sprite":"BossFightTriggGO","x":2224,"y":523,
-				 "br1posx":-2301,"br1posy":-111,"br2posx":631,"br2posy":-111,"spriteId":"BossFightTriggGO"
-				},
-				{"sprite":"falseknight","x":1289,"y":-367,"width":368,"height":223,"spriteId":"boss_fknight1"}
+				// {"sprite":"BossFightTriggGO","x":2224,"y":523,
+				//  "br1posx":-2301,"br1posy":-111,"br2posx":631,"br2posy":-111,"spriteId":"BossFightTriggGO"
+				// },
+				// {"sprite":"falseknight","x":1289,"y":-367,"width":368,"height":223,"spriteId":"boss_fknight1"}
 			],
 			
 			[
-			    // {"sprite":"player","x":200,"y":200,"width":150,"height":100},
+			    {"sprite":"shrub","x":-113,"y":932,"width":1362,"height":250,"xoffset":0,"yoffset":0},
+			    {"sprite":"shrub","x":450,"y":1256,"width":1171,"height":171,"xoffset":0,"yoffset":0},
+			    {"sprite":"thorn","x":1225,"y":1240,"width":903,"height":250,"xoffset":0,"yoffset":0},
+			    {"sprite":"shrub","x":4562,"y":1657,"width":1561,"height":171,"xoffset":0,"yoffset":0},
+			    {"sprite":"thorn","x":3313,"y":1626,"width":1107,"height":250,"xoffset":0,"yoffset":0},
 				{"sprite":"platform","x":200,"y":600,"width":2200,"height":50},		
 				{"sprite":"platform","x":2363,"y":1000,"width":2200,"height":50},	
 				{"sprite":"platform","x":3576,"y":1179,"width":250,"height":50},
