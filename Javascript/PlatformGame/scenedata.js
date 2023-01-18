a9574da3548a15ee4375ca2d63bb4c4bfdea6558
unknown <km123abca>
2023-01-18 07:36:39 +0530

    commit made at  7:36:38.84 date:18-01-2023

--- Javascript/PlatformGame/scenedata.js
+++ Javascript/PlatformGame/scenedata.js
@@ -1,7 +1,7 @@
 let scenes=[
 			[
-				{"sprite":"platform","x":150,"y":-160,"width":50,"height":1280},
-				{"sprite":"WallGripper","x":125,"y":-177,"width":50,"height":1304},
+				// {"sprite":"platform","x":150,"y":-160,"width":50,"height":1280},
+				// {"sprite":"WallGripper","x":125,"y":-177,"width":50,"height":1304},
 			    {"sprite":"thorn","x":1870,"y":924,"width":600,"height":250,"xoffset":0,"yoffset":0},
 			    {"sprite":"shrub","x":200,"y":924,"width":2400,"height":250,"xoffset":0,"yoffset":0},
 			    {"sprite":"shrub","x":3675,"y":924,"width":2400,"height":250,"xoffset":0,"yoffset":0},
