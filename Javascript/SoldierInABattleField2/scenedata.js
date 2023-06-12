59fff6055a0b442d23fc21099bc997e702cf74b4
unknown <km123abca>
2023-06-12 08:22:50 +0530

    commit made at  8:22:50.07 date:12-06-2023

---
+++ Javascript/SoldierInABattleField2/scenedata.js
@@ -0,0 +1,6 @@
+let scenes=[
+            [                               
+            {"sprite":"player","x":400,"y":300,"width":100,"height":100},  
+            {"sprite":"wall","x":150,"y":-210,"width":50,"height":1280,"hitboxoffsetx":0,"hitboxoffsety":0,"isHori":true},                            
+            ],
+           ];
