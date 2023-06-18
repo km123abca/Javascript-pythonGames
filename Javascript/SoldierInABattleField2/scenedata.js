0e2c4769a07c827b46e9c818556ffab61210302e
Krishnamohan M <km123abca@gmail.com>
2023-06-18 16:13:40 +0530

    commit made at 16:13:39.39 date:18-06-2023

--- Javascript/SoldierInABattleField2/scenedata.js
+++ Javascript/SoldierInABattleField2/scenedata.js
@@ -1,6 +1,24 @@
-let scenes=[
+let scenes=[            
+            [   
+            {"sprite":"entryexit_entry","x":62,"y":-203}, 
+            {"sprite":"entryexit_exit","x":1128,"y":854},                           
+            {"sprite":"player","x":400,"y":300,"width":100,"height":100},  
+            {"sprite":"wall","x":-100,"y":300,"width":50,"height":1280,
+             "hitboxoffsetx":0,"hitboxoffsety":0,"isHori":false},  
+            {"sprite":"wall","x":1275,"y":104,"width":50,"height":1382,
+             "hitboxoffsetx":0,"hitboxoffsety":0,"isHori":false},
+            {"sprite":"wall","x":663,"y":-341,"width":1608,"height":52,
+             "hitboxoffsetx":0,"hitboxoffsety":0,"isHori":true}, 
+            {"sprite":"wall","x":375,"y":458,"width":1280,"height":50,
+             "hitboxoffsetx":0,"hitboxoffsety":0,"isHori":true},  
+            {"sprite":"wall","x":563,"y":942,"width":1380,"height":50,
+             "hitboxoffsetx":0,"hitboxoffsety":0,"isHori":true},                         
+            ],
             [                               
             {"sprite":"player","x":400,"y":300,"width":100,"height":100},  
-            {"sprite":"wall","x":150,"y":-210,"width":50,"height":1280,"hitboxoffsetx":0,"hitboxoffsety":0,"isHori":true},                            
+            {"sprite":"wall","x":663,"y":156,"width":1608,"height":52,
+             "hitboxoffsetx":0,"hitboxoffsety":0,"isHori":true},
+             {"sprite":"wall","x":663,"y":392,"width":1608,"height":52,
+             "hitboxoffsetx":0,"hitboxoffsety":0,"isHori":true},                                   
             ],
            ];
