b5cd22c8d565be1b1f97b2c9c7485ca06bb21359
Krishnamohan M <km123abca@gmail.com>
2024-08-31 18:39:19 +0530

    Erratic behaviour of camera need to be solved. on 18:39:15.90 date:31-08-2024

--- autogit.bat
+++ autogit.bat
@@ -1,5 +1,26 @@
-echo off
+@echo off
+setlocal
+
+:: Prompt the user for a commit message
+set /p userMessage="Enter commit message (leave blank for default message): "
+
+:: Check if the user entered a message
+if "%userMessage%"=="" (
+    :: No message entered, use the default message
+    set commitMessage="commit made at %time% date:%date%"
+) else (
+    :: User entered a message, use it for the commit
+    set commitMessage=%userMessage% on %time% date:%date%
+)
+
+:: Add all changes to the staging area
 git add .
-git commit -m "commit made at %time% date:%date%"
+
+:: Commit with the determined message
+git commit -m "%commitMessage%"
+
+:: Push changes to the master branch
 git push origin master
+
+:: Pause the script to view the output
 pause
