a30f7c679357527026250856aea4478a647a1d14
Krishnamohan M <km123abca@gmail.com>
2024-10-06 19:09:07 +0530

    commit_made_at_19:09:06.34_date:06-10-2024

---
+++ Javascript/SoldierInABattleField2/map.js
@@ -0,0 +1,74 @@
+let mapx=[
+  {
+    "img": {},
+    "wid": 400,
+    "hei": 40,
+    "sprite_type": "HorizontalPipe",
+    "x": 229,
+    "y": 44
+  },
+  {
+    "img": {},
+    "wid": 400,
+    "hei": 40,
+    "sprite_type": "HorizontalPipe",
+    "x": 624,
+    "y": 44
+  },
+  {
+    "img": {},
+    "wid": 224,
+    "hei": 40,
+    "sprite_type": "HorizontalPipe",
+    "x": 933,
+    "y": 44
+  },
+  {
+    "img": {},
+    "wid": 400,
+    "hei": 40,
+    "sprite_type": "HorizontalPipe",
+    "x": 237,
+    "y": 506
+  },
+  {
+    "img": {},
+    "wid": 400,
+    "hei": 40,
+    "sprite_type": "HorizontalPipe",
+    "x": 633,
+    "y": 506
+  },
+  {
+    "img": {},
+    "wid": 226,
+    "hei": 40,
+    "sprite_type": "HorizontalPipe",
+    "x": 943,
+    "y": 506
+  },
+  {
+    "img": {},
+    "wid": 46,
+    "hei": 303,
+    "sprite_type": "VerticalPipe",
+    "x": 1025,
+    "y": 216
+  },
+  {
+    "img": {},
+    "wid": 56,
+    "hei": 400,
+    "sprite_type": "VerticalPipe",
+    "x": 65,
+    "y": 286
+  },
+  {
+    "img": {},
+    "wid": 56,
+    "hei": 41,
+    "sprite_type": "VerticalPipe",
+    "x": 65,
+    "y": 81
+  }
+];
