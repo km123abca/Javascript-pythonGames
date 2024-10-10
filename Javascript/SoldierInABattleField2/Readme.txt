7be9b166e34576f49ee7cae5c5b832afb09ffa26
Krishnamohan M <km123abca@gmail.com>
2024-10-10 08:38:40 +0530

    outputting with created canvas dims on  8:38:35.92 date:10-10-2024

---
+++ Javascript/SoldierInABattleField2/Readme.txt
@@ -0,0 +1,2 @@
+Commit comments
+#topvieweditor: complete till placing indicator objects on 07-10-2024
---
+++ Javascript/SoldierInABattleField2/tileeditor_helper.js
@@ -0,0 +1,96 @@
+const buildSceneData=()=>{
+    let output_map=[];
+    let cw=canvas.width,ch=canvas.height;
+    for(let map_elem of this.map)
+        {
+        if(map_elem.sprite_type=="VerticalPipe" )
+            {
+               output_map.push({"sprite":"wall",
+                                "x":map_elem.x,
+                                "y":map_elem.y,
+                                "width":map_elem.wid,
+                                "height":map_elem.hei,
+                                "hitboxoffsetx":0,
+                                "hitboxoffsety":0,
+                                "isHori":false,
+                                "canvas_width":cw,
+                                "canvas_height":ch
+                                }); 
+            }
+        else if(map_elem.sprite_type=="HorizontalPipe")
+            {
+               output_map.push({"sprite":"wall",
+                                "x":map_elem.x,
+                                "y":map_elem.y,
+                                "width":map_elem.wid,
+                                "height":map_elem.hei,
+                                "hitboxoffsetx":0,
+                                "hitboxoffsety":0,
+                                "isHori":true,"canvas_width":cw,
+                                "canvas_height":ch}); 
+            }
+        else if(map_elem.sprite_type=="Coin" && map_elem.indicatorDesc=="entry")
+            {
+               output_map.push({"sprite":"entryexit_entry",
+                                "x":map_elem.x,
+                                "y":map_elem.y,,"canvas_width":cw,
+                                "canvas_height":ch
+                                }); 
+            }
+        else if(map_elem.sprite_type=="Coin" && map_elem.indicatorDesc=="exit")
+            {
+               output_map.push({"sprite":"entryexit_exit",
+                                "x":map_elem.x,
+                                "y":map_elem.y,,"canvas_width":cw,
+                                "canvas_height":ch
+                                }); 
+            }
+        else if(map_elem.sprite_type=="Coin" && map_elem.indicatorDesc=="camstopper_left")
+            {
+               output_map.push({"sprite":"camstopper_left",
+                                "x":map_elem.x,
+                                "y":map_elem.y,,"canvas_width":cw,
+                                "canvas_height":ch
+                                }); 
+            }
+        else if(map_elem.sprite_type=="Coin" && map_elem.indicatorDesc=="camstopper_right")
+            {
+               output_map.push({"sprite":"camstopper_right",
+                                "x":map_elem.x,
+                                "y":map_elem.y,,"canvas_width":cw,
+                                "canvas_height":ch
+                                }); 
+            }
+        else if(map_elem.sprite_type=="Coin" && map_elem.indicatorDesc=="scright")
+            {
+               output_map.push({"sprite":"scenechanger_scright",
+                                "x":map_elem.x,
+                                "y":map_elem.y,,"canvas_width":cw,
+                                "canvas_height":ch
+                                }); 
+            }
+        else if(map_elem.sprite_type=="Coin" && map_elem.indicatorDesc=="scleft")
+            {
+               output_map.push({"sprite":"scenechanger_scleft",
+                                "x":map_elem.x,
+                                "y":map_elem.y,,"canvas_width":cw,
+                                "canvas_height":ch
+                                }); 
+            }
+        else if(map_elem.sprite_type=="Coin" && map_elem.indicatorDesc=="flower")
+            {
+               output_map.push({"sprite":"flower",
+                                "x":map_elem.x,
+                                "y":map_elem.y,
+                                "width":50,
+                                "height":50,"canvas_width":cw,
+                                "canvas_height":ch
+                                }); 
+            }
+
+
+        }
+        const popupWindow = window.open("", "popup", "width=300,height=200");
+        popupWindow.document.write(`<p>${JSON.stringify(outputJson)}</p>`);
+        popupWindow.document.title = "Your Design";
+}
